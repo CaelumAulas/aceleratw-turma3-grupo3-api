f1f8018abfc7c50bc63138a6c90f3683fd86ddda
William Ribeiro Martins <william.martins@Williams-MacBook-Pro.local>
2021-06-10 10:09:56 -0300

    base do projeto de back

---
+++ src/main/java/br/com/carangobom/carangoBom/CarangoBomApplication.java
@@ -0,0 +1,13 @@
+package br.com.carangobom.carangoBom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarangoBomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarangoBomApplication.class, args);
+	}
+
+}
