3dc3caa5e12835cf9c8926b9cde94d19ecbc92be
lajoaomatheus.guedes <joaoguedesmg@gmail.com>
2021-06-14 09:17:09 -0300

    Adding SQL integrity handler

--- src/main/java/br/com/carangobom/carangoBom/repository/BrandRepository.java
+++ src/main/java/br/com/carangobom/carangoBom/repository/BrandRepository.java
@@ -1,14 +1,13 @@
 package br.com.carangobom.carangoBom.repository;
 
 import br.com.carangobom.carangoBom.models.Brand;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
+
 
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
-    //Page<Brand> findByName(String name, Pageable pages);
+
+
 
 }
