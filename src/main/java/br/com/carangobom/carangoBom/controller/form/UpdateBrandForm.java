be4729548bfa7a6c3027d3e2d6654ee673f23dda
lajoaomatheus.guedes <joaoguedesmg@gmail.com>
2021-06-15 16:46:26 -0300

    Create and update brands

---
+++ src/main/java/br/com/carangobom/carangoBom/controller/form/UpdateBrandForm.java
@@ -0,0 +1,28 @@
+package br.com.carangobom.carangoBom.controller.form;
+
+import br.com.carangobom.carangoBom.model.Brand;
+import br.com.carangobom.carangoBom.repository.BrandRepository;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Optional;
+
+public class UpdateBrandForm {
+
+
+    @NotNull @NotEmpty
+
+    private String name;
+
+    public UpdateBrandForm(String name) {
+        this.name = name;
+    }
+
+    public Brand updateBrand(Long id, BrandRepository brandRepository) {
+        Optional<Brand> brand = brandRepository.findById(id);
+        brand.get().setName(this.name);
+
+        return brand.get();
+
+
+    }
+}
