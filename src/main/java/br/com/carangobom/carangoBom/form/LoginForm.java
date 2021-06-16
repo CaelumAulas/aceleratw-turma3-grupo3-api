3a688ce20e0ed6f1d7497b8c93fbcc77d69db6f1
William Ribeiro Martins <william.martins@Williams-MacBook-Pro.local>
2021-06-16 16:52:18 -0300

    Teste unitario da UserController e usando o lombok na classe LoginForm

--- src/main/java/br/com/carangobom/carangoBom/form/LoginForm.java
+++ src/main/java/br/com/carangobom/carangoBom/form/LoginForm.java
@@ -1,10 +1,12 @@
 package br.com.carangobom.carangoBom.form;
 
+import lombok.Data;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@Data
 public class LoginForm {
 
     @NotEmpty
@@ -14,22 +16,6 @@ public class LoginForm {
     @NotEmpty
     @NotNull
     private String password;
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
 
     public UsernamePasswordAuthenticationToken converter() {
         return new UsernamePasswordAuthenticationToken(email, password);
---
+++ src/test/java/br/com/carangobom/carangoBom/controller/UserControllerTest.java
@@ -0,0 +1,39 @@
+package br.com.carangobom.carangoBom.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import java.net.URI;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldReturnForbiddenWhenGetUsers() throws Exception {
+        URI uri = new URI("/users");
+        String json = "{\"user\":\"invalido@email.com\",\"password\":\"123456\"}";
+
+        mockMvc
+            .perform(MockMvcRequestBuilders
+                    .post(uri)
+                    .content(json)
+                    .contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers
+                    .status()
+                    .is(403));
+    }
+}
