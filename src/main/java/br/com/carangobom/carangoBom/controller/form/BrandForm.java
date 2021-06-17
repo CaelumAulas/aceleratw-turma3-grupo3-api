bf60171362f7463c5597a5dd7b682b0992395614
joaoguedesmg <joaoguedesmg@gmail.com>
2021-06-17 16:13:52 -0300

    Update BrandForm.java

--- src/main/java/br/com/carangobom/carangoBom/controller/form/BrandForm.java
+++ src/main/java/br/com/carangobom/carangoBom/controller/form/BrandForm.java
@@ -1,23 +1,25 @@
-package br.com.carangobom.carangoBom.controller.form;
+package br.com.carangobom.carangoBom.form;
 
 import br.com.carangobom.carangoBom.model.Brand;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-
+@Getter
+@NoArgsConstructor
 public class BrandForm {
-
     @NotNull
     @NotEmpty
+
     private String name;
 
-    public BrandForm( String name) {
+    public BrandForm(String name) {
         this.name = name;
     }
 
     public Brand convert() {
-
-
         return new Brand(name);
     }
 }
