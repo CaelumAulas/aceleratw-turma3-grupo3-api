ac4dadb9f74cad091132e27b024c74078c6c56a7
lajoaomatheus.guedes <joaoguedesmg@gmail.com>
2021-06-18 10:41:02 -0300

    Update UserForm.java

--- src/main/java/br/com/carangobom/carangoBom/form/UserForm.java
+++ src/main/java/br/com/carangobom/carangoBom/form/UserForm.java
@@ -4,6 +4,8 @@ import br.com.carangobom.carangoBom.model.User;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
@@ -21,7 +23,9 @@ public class UserForm {
     @Length(min = 6)
     private String password;
 
-    public static User converter(UserForm user) {
-        return new User(user.getUser(), user.getPassword());
+    public static User converter(UserForm userform) {
+        userform.setPassword(new BCryptPasswordEncoder().encode(userform.password));
+        return new User(userform.getUser(), userform.getPassword());
     }
 }
+
